bddbd2f688b8c3aca6510c9a1229f6e0da791b82
minsung Joh <jms5049@naver.com>
2018-09-24 00:57:49 +0900

    15686-01

---
+++ BOJ/p15686.java
@@ -0,0 +1,24 @@
+﻿import java.util.*;
+
+class point{
+    int x;
+    int y;
+    public point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+public class p15686 {
+    static int N;
+    static int M;
+    static int[][] map;
+    static int answer = Integer.MAX_VALUE;
+    static ArrayList<point> chicken= new ArrayList<point>();
+    static ArrayList<point> temp= new ArrayList<point>();
+    static ArrayList<point> house= new ArrayList<point>();
+    
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        
+        
